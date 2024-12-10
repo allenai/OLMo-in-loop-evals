ab70c28fbba394e592f38821d3cdf283fb639c07
Jiacheng Liu <liujch1998@gmail.com>
2024-12-10 07:23:08 +0000

    Sync eval changes in OLMo/ladder-1xC to here

--- src/olmo_eval/metrics.py
+++ src/olmo_eval/metrics.py
@@ -19,12 +19,16 @@ class ICLMetric(Metric):
         self.metric_type = metric_type
 
         self.add_state("loglikelihoods", default=[], dist_reduce_fx=None)
+        self.add_state("celosses", default=[], dist_reduce_fx=None)
+        self.add_state("bpbs", default=[], dist_reduce_fx=None)
         self.add_state("labels", default=[], dist_reduce_fx=None)
 
     def reset(
         self,
     ):
         self.loglikelihoods = []
+        self.celosses = []
+        self.bpbs = []
         self.labels = []
 
     def update(self, batch: Dict[str, Any], lm_logits: torch.Tensor, dc_lm_logits=None):
@@ -46,6 +50,8 @@ class ICLMetric(Metric):
             ]
 
             log_likelihood: torch.Tensor
+            celoss: torch.Tensor
+            bpb: torch.Tensor
             if self.metric_type == "pmi_dc":
                 assert dc_lm_logits is not None
                 # get domain conditional continuation logits: [cont_len, vocab]
@@ -58,19 +64,28 @@ class ICLMetric(Metric):
                     torch.gather(lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum()
                     / torch.gather(dc_lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum()
                 )
+                celoss = -log_likelihood
+                bpb = -log_likelihood  # the normalization factors cancel out
             elif self.metric_type == "acc" or self.metric_type == "f1":
                 # gather log-probs at continuation token indices
                 log_likelihood = torch.gather(lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum()
-            elif self.metric_type == "len_norm" or self.metric_type == "ce_loss":
+                celoss = (
+                    -torch.gather(lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum() / batch["cont_str_len"][idx]
+                )
+                bpb = (
+                    -torch.gather(lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum()
+                    / batch["cont_byte_len"][idx]
+                    * LOG_2_OF_E
+                )
+            elif self.metric_type in ["len_norm", "ce_loss", "bpb"]:
                 log_likelihood = (
                     torch.gather(lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum()
                     / batch["cont_str_len"][idx]
                 )
-                if self.metric_type == "ce_loss":
-                    log_likelihood = -log_likelihood
-            elif self.metric_type == "bpb":
-                # bits per byte
-                log_likelihood = (
+                celoss = (
+                    -torch.gather(lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum() / batch["cont_str_len"][idx]
+                )
+                bpb = (
                     -torch.gather(lm_cont_logits, 1, cont_tokens.unsqueeze(-1)).sum()
                     / batch["cont_byte_len"][idx]
                     * LOG_2_OF_E
@@ -84,16 +99,22 @@ class ICLMetric(Metric):
                     batch["continuation"][idx].device
                 )
             )
+            self.celosses.append(torch.Tensor((doc_id, cont_id, celoss)).to(batch["continuation"][idx].device))
+            self.bpbs.append(torch.Tensor((doc_id, cont_id, bpb)).to(batch["continuation"][idx].device))
             self.labels.append(
                 torch.LongTensor((doc_id, cont_id, batch["label_id"][idx])).to(
                     batch["label_id"][idx].device
                 )
             )
 
-    def compute(self) -> torch.Tensor:
+    def compute(self) -> Dict[str, torch.Tensor]:
+        # Task "suffix" -> tensor
+
         # states should have been synced from all accelerators at this point
         # account for duplicates here because of DistributedSampler compensating for drop_last=False
         loglikelihood_dict: Dict[int, Dict[int, float]] = {}
+        celoss_dict: Dict[int, Dict[int, float]] = {}
+        bpb_dict: Dict[int, Dict[int, float]] = {}
         label_dict = {}
 
         # collect labels
@@ -109,8 +130,29 @@ class ICLMetric(Metric):
             if int(cont_id.item()) not in loglikelihood_dict[int(doc_id.item())]:
                 loglikelihood_dict[int(doc_id.item())][int(cont_id.item())] = loglikelihood
 
+        # collect celosses
+        for doc_id, cont_id, celoss in self.celosses:
+            if int(doc_id.item()) not in celoss_dict:
+                celoss_dict[int(doc_id.item())] = {}
+
+            if int(cont_id.item()) not in celoss_dict[int(doc_id.item())]:
+                celoss_dict[int(doc_id.item())][int(cont_id.item())] = celoss
+
+        # collect bpbs
+        for doc_id, cont_id, bpb in self.bpbs:
+            if int(doc_id.item()) not in bpb_dict:
+                bpb_dict[int(doc_id.item())] = {}
+
+            if int(cont_id.item()) not in bpb_dict[int(doc_id.item())]:
+                bpb_dict[int(doc_id.item())][int(cont_id.item())] = bpb
+
         # compute acc
         correct = []
+        loglikelihood = []
+        celoss = []
+        bpb = []
+        soft_score = []
+        soft_log_score = []
         preds: Optional[List[float]] = None
         labels: Optional[List[int]] = None
         if self.metric_type == "f1":
@@ -121,11 +163,15 @@ class ICLMetric(Metric):
             # each doc_id might have a different number of continuation
             num_continuations = len(loglikelihood_dict[doc_id].keys())
             loglikelihoods = torch.tensor([-float("inf")] * num_continuations)
+            celosses = torch.tensor([float("inf")] * num_continuations)
+            bpbs = torch.tensor([float("inf")] * num_continuations)
 
             skip_document = False
             for cont_id in loglikelihood_dict[doc_id]:
                 try:
                     loglikelihoods[cont_id] = loglikelihood_dict[doc_id][cont_id]
+                    celosses[cont_id] = celoss_dict[doc_id][cont_id]
+                    bpbs[cont_id] = bpb_dict[doc_id][cont_id]
                 except IndexError:
                     # We didn't process all of the continuations, so skip this document.
                     skip_document = True
@@ -133,25 +179,37 @@ class ICLMetric(Metric):
 
             if skip_document:
                 continue
-            if self.metric_type in ["ce_loss", "bpb"]:
-                correct.append(loglikelihoods[0])  # Only one answer is scored
-            else:
-                correct.append(
-                    1.0 if torch.argmax(loglikelihoods).item() == label_dict[doc_id] else 0.0
-                )
-
-            if self.metric_type == "f1":
+            if self.metric_type == "ce_loss":
+                celoss.append(celosses[0])  # Only one answer is scored
+            elif self.metric_type == "bpb":
+                bpb.append(bpbs[0])  # Only one answer is scored
+            elif self.metric_type == "f1":
                 assert preds is not None
                 assert labels is not None
                 preds.append(torch.argmax(loglikelihoods).item())
                 labels.append(label_dict[doc_id])
+            else:
+                correct.append(1.0 if torch.argmax(loglikelihoods).item() == label_dict[doc_id] else 0.0)
+                celoss.append(celosses[label_dict[doc_id]].item())
+                bpb.append(bpbs[label_dict[doc_id]].item())
+                soft_score.append(torch.softmax(loglikelihoods, dim=0)[label_dict[doc_id]].item())
+                soft_log_score.append(torch.log_softmax(loglikelihoods, dim=0)[label_dict[doc_id]].item())
 
         if self.metric_type == "f1":
             assert preds is not None
             assert labels is not None
             # for NLI tasks, continuations are yes, no, neither, so idx=0 assigned to pos label
             score = f1_score(labels, preds, pos_label=0)
+            return {"f1": torch.tensor(score)}
+        elif self.metric_type == "ce_loss":
+            return {"ce_loss": torch.tensor(sum(celoss) / len(celoss))}
+        elif self.metric_type == "bpb":
+            return {"bpb": torch.tensor(sum(bpb) / len(bpb))}
         else:
-            score = sum(correct) / len(correct)
-
-        return torch.tensor(score)
+            return {
+                self.metric_type: torch.tensor(sum(correct) / len(correct)),
+                "ce_loss": torch.tensor(sum(celoss) / len(celoss)),
+                "bpb": torch.tensor(sum(bpb) / len(bpb)),
+                "soft": torch.tensor(sum(soft_score) / len(soft_score)),
+                "soft_log": torch.tensor(sum(soft_log_score) / len(soft_log_score)),
+            }
